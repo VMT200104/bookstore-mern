6253ab5684b9c2df1887c4ce6bde95b78f3c18d4
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 22:41:50 +0700

    update server new

--- server/app.js
+++ server/app.js
@@ -20,7 +20,10 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));  
 app.use(cookieParser());
 app.use(fileUpload({ useTempFiles: true }));
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // hoặc domain frontend của bạn
+  credentials: true,
+}));
 
 const port = process.env.PORT;
 
