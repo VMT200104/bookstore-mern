dda7b379517a3adf87a3b9706759933d7ffe1b2b
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 13:39:56 +0700

    update package.json

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { allUsersReducer, forgotPasswordReducer, profileReducer, userDetailsReducer, userReducer } from "./reducers/userReducer";
 import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer, productReviewsReducer, productsReducer, reviewReducer } from "./reducers/productReducer";
