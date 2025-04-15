7ab9f39aa791c1699412b3aeac871ea74656e5b1
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 22:32:10 +0700

    Cập nhật code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-axios.defaults.withCredentials = true;
+
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
