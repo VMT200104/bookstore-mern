3d91fece6eac31ae12579ce3db6649310d07090c
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 22:02:56 +0700

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,10 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
