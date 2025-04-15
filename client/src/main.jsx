e9029864631f26e560e28b87ab8a8f4d2a083cb8
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 23:01:02 +0700

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
