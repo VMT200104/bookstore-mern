961fe8ca4a3fc99a48a8055a90796d946d6b3a7b
VMT200104 <minhtandragon29@gmail.com>
2025-04-15 21:13:02 +0700

    update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Cổng của backend
+        target: "https://bookstore-mern-lh17.onrender.com", // Cổng của backend
         changeOrigin: true,
         secure: false,
       },
