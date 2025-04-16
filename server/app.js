5b6bc328cef43c725681c6308788e82473ce1e4f
VMT200104 <minhtandragon29@gmail.com>
2025-04-16 19:03:59 +0700

    update app.js

--- server/app.js
+++ server/app.js
@@ -22,12 +22,10 @@ app.use(cookieParser());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(cors({
   origin: [
-    "https://bookstore-mern-livid.vercel.app/",
+    "https://bookstore-mern-1-5sr8.onrender.com",
     "http://localhost:5173"
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'token']
 }));
 
 const port = process.env.PORT;
