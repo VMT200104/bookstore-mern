1d85a1aa6571d960d81d1b937b4a832b4d4c11fb
VMT200104 <minhtandragon29@gmail.com>
2025-04-16 18:56:11 +0700

    update main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
