40f1e9e1622b18368f105420f7bfc2215bfa1194
VMT200104 <minhtandragon29@gmail.com>
2025-04-16 16:47:42 +0700

    update server new

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-client-domain.com'],
+  origin: ['http://localhost:5173', 'https://bookstore-mern-1-5sr8.onrender.com', 'https://bookstore-mern-lh17.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'token']
