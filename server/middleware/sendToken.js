4ffb7b6786deb0c4ca2a39c8f1f5889c6d536b69
VMT200104 <minhtandragon29@gmail.com>
2025-04-16 16:30:02 +0700

    update sendToken

--- server/middleware/sendToken.js
+++ server/middleware/sendToken.js
@@ -5,7 +5,9 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
     }
 
     res.status(statusCode).cookie('token', token, options).json({
