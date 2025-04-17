861c47564135a002d6576dc46d82ae7506808824
VMT200104 <minhtandragon29@gmail.com>
2025-04-17 18:58:41 +0700

    update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -25,5 +25,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
